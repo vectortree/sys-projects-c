b6ec8e7221f44c7be2c096758272664aa22efd5a
Starr Xu <staxu@cs.stonybrook.edu>
2022-05-04 14:45:28 -0400

    Implemented main.c

--- hw5/src/main.c
+++ hw5/src/main.c
@@ -4,8 +4,11 @@
 #include "pbx.h"
 #include "server.h"
 #include "debug.h"
+#include "csapp.h"
 
 static void terminate(int status);
+static void sighup_handler(int sig);
+static void *thread(void *vargp);
 
 /*
  * "PBX" telephone exchange simulation.
@@ -16,9 +19,23 @@ int main(int argc, char* argv[]){
     // Option processing should be performed here.
     // Option '-p <port>' is required in order to specify the port number
     // on which the server should listen.
-
+    char opt, flag = 0;
+    char *port;
+    while((opt = getopt(argc, argv, "p:")) != -1) {
+        switch(opt) {
+            case 'p':
+                if(atoi(optarg) > 0) {
+                    port = optarg;
+                    flag = 1;
+                }
+        }
+    }
+    if(!flag) {
+        fprintf(stderr, "Usage: %s %s\n", argv[0], "-p <port>");
+        exit(EXIT_FAILURE);
+    }
     // Perform required initialization of the PBX module.
-    debug("Initializing PBX...");
+    debug("Initializing PBX...\n");
     pbx = pbx_init();
 
     // TODO: Set up the server socket and enter a loop to accept connections
@@ -26,6 +43,27 @@ int main(int argc, char* argv[]){
     // run function pbx_client_service().  In addition, you should install
     // a SIGHUP handler, so that receipt of SIGHUP will perform a clean
     // shutdown of the server.
+
+    // Install SIGHUP signal handler.
+    struct sigaction sa;
+    sa.sa_handler = sighup_handler;
+    sigaction(SIGHUP, &sa, NULL);
+
+    // NOTE: We are allowed to use code snippets from the textbook and/or slides.
+    // This is for setting up the server socket and entering a loop
+    // to accept connections on this socket.
+    int listenfd, *connfdp;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    pthread_t tid;
+
+    listenfd = Open_listenfd(port);
+    while(1) {
+        clientlen = sizeof(struct sockaddr_storage);
+        connfdp = Malloc(sizeof(int));
+        *connfdp = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        Pthread_create(&tid, NULL, thread, connfdp);
+    }
 
     fprintf(stderr, "You have to finish implementing main() "
 	    "before the PBX server will function.\n");
@@ -41,4 +79,20 @@ static void terminate(int status) {
     pbx_shutdown(pbx);
     debug("PBX server terminating");
     exit(status);
+}
+
+/*
+ * This is the SIGHUP handler. Upon receipt of a
+ * SIGHUP signal, it terminates the server cleanly.
+ */
+static void sighup_handler(int sig) {
+    terminate(EXIT_SUCCESS);
+}
+
+/*
+ * For each connection, a thread should be started to
+ * run function pbx_client_service().
+ */
+static void *thread(void *vargp) {
+    return pbx_client_service(vargp);
 }
